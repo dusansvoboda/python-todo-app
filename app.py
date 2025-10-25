5e1da71f012195452f7e66825b7c0646c95f7ba1
dusansvoboda <dusan.svoboda@gmail.com>
2025-10-25 16:41:58 -0400

    test CI fix and update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # We'll change this text later to prove our pipeline works
-    return "Hello from our K3s Cluster! Version 1.0\n"
+    return "Hello from our K3s Cluster! Version 1.0 (Testing CI)\n\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
