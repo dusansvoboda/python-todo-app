57221e4262a04e3e0990f4fb76f50d95781a953f
dusansvoboda <dusan.svoboda@gmail.com>
2025-10-25 17:33:25 -0400

    testing pipeline

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # We'll change this text later to prove our pipeline works
-    return "Hello from our K3s Cluster and CI CD! Version 2.0 🚀\n\n"
+    return "Hello from our K3s Cluster and CI CD! Version 3.0 🚀\n\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
